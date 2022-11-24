4c9e27bdbf9b7cfc9da1c53be202a124ec49a55a
TIKAY <tikamante@gmail.com>
2022-11-24 23:55:32 +0000

    use cases implemented

--- app/src/main/java/com/tikay/homitest/Config.java
+++
@@ -1,5 +0,0 @@
-package com.tikay.homitest;
-
-public interface Config {
-    float scale = 10f / 3f;
-}
---
+++ app/src/main/java/com/tikay/homitest/domain/usecase/series/GetAllSeriesUseCase.java
@@ -0,0 +1,2 @@
+package com.tikay.homitest.domain.usecase.series;public class GetAllSeriesUseCase {
+}
